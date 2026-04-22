3e1c292ae4709a5bf2836db7090b6b32767e9e99
Tanvir286 <tanvirdiu200@gmail.com>
2026-04-22 12:10:22 +0600

    structure ready admin dashborad

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { ContactModule } from './contact/contact.module';
 import { PaymentTransactionModule } from './payment-transaction/payment-transaction.module';
 import { UserModule } from './user/user.module';
 import { NotificationModule } from './notification/notification.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { NotificationModule } from './notification/notification.module';
     PaymentTransactionModule,
     UserModule,
     NotificationModule,
+    DashboardModule,
   ],
 })
 export class AdminModule {}
